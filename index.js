64945cc3c8f374236abde48fea916508281ce1a3
wxnet <wxnet@wxnets-MacBook-Pro.local>
2017-03-31 14:57:24 +0800

    refactor(check): 修改类型检查器

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-import { encode } from 'querystring';
+import pathParams from './src/pathparams';
+import combineQuery from './src/combinequery';
+import check from './src/check';
 
-const schemaTypes = {
-  number: Number,
-  string: String,
-  array: Array,
-  boolean: Boolean,
-};
-
-if (!Array.isArray) {
-  Array.isArray = (obj) => Object.prototype.toString.call(obj) === '[object Array]';
-}
-
-const getType = (value) => {
-  if (typeof value === 'number') return 'number';
-  if (typeof value === 'string') return 'string';
-  if (typeof value === 'boolean') return 'boolean';
-  if (Array.isArray(value)) return 'array';
-  return '';
-};
-
-export const apischema = (config) => {
+export default (config) => {
   const io = config.http;
   // io.get => Promise
   // io.post => Promise
@@ -28,28 +11,19 @@ export const apischema = (config) => {
   return {
     combineUrl(method, url, params) {
       // 替换path参数
-      let combineUrl = url.replace(/\{([a-zA-Z]*)\}/g, ($1, $2) => {
-        console.log($1, $2);
-        return params[$2];
-      });
+      let combineUrl = pathParams(url, params);
       if (method === 'get') {
-        const query = encode(params);
-        const split = url.indexOf('?') === -1 ? '?' : '&';
-        combineUrl = query ? `${combineUrl}${split}${query}` : combineUrl;
+        combineUrl = combineQuery(combineUrl, params);
       }
       return combineUrl;
     },
     define(url, schema = {}, method = 'get') {
       return (params) => {
+        // 验证请求参数的合法性
+        check(schema, params);
         // 最终发送给接口的参数
         const sendParams = {};
-        // 验证请求参数的合法性
         Object.keys(schema).forEach((param) => {
-          if (schema[param].required &&
-            params[param] === void 0) throw new TypeError(`缺少参数：请添加${param}参数`);
-          if (schema[param].type !== schemaTypes[getType(params[param])]) {
-            throw new TypeError(`类型错误：参数${param}类型错误`);
-          }
           // 如果只是用于接口URL中的参数，则不加入进最终发送给接口的参数
           if (!schema[param].urlOnly) {
             sendParams[param] = params[param];
